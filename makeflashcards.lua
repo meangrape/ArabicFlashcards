fedaceb8302b06628fda48f300b3bf4eb5c3b27c
Jay <jay@meangrape.com>
2020-07-09 17:26:30 -0400

    name lua function MakeFlashCards instead of makeflash cards; hoping this brings things into line with the `return { makeflashcards = MakeFlashCards}`. Which is to be inline with the LuaLatex definition of `\makeflashcards`

--- makeflashcards.lua
+++ makeflashcards.lua
@@ -1,7 +1,7 @@
 -- loops through "arabic, english" pairs (one per line) from a file that defaults to dictionary.txt
 -- and prints out a LuaTex flashcard block for each
 
-function makeflashcards()
+function MakeFlashCards()
   local csvfile = require("./luarocks/5.3/lua-csv/csv")
   local data = csvfile.open("dictionary.csv")
   local arabic
